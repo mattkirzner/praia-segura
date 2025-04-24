486ebcd51d2a4783a2c8a8f0f1568c23557824fd
Arthur Borba Lins <arthurborbalins@gmail.com>
2025-04-24 15:13:45 -0300

    Adicionando pastas de componentes e páginas, atualizando App.jsx
    Ajuste no App.jsx e App.css;
    criação de uma pasta pra componentes, onde estão o TopBar e BotBar .jsx e .css;
    criação de uma pasta para as páginas, mas, até então, apenas os arquivos .jsx vazios.

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
 import * as React from 'react';
-import './App.css'
-import gov from './imgs/govpe.png';
-import logo from './imgs/pslogo.png';
-import brasil from './imgs/brasil.png';
-import eua from './imgs/eua.png'
-import ouvidoria from './imgs/icon-ouvidoria.jpg'
-
-const TopBar = () =>{
-  return(
-    
-    <div id='topbar'>
-    <img src={gov} className='topimg' id='gov' alt="" />
-    <img src={logo} className='topimg' id='logo' alt="" />
-    <span className='langs'><img src={brasil} className='topimg' alt="" /><img  src={eua} className='topimg' alt="" /></span>
-    </div>
-    
-  )
-
-};
-
-const BotBar = () =>{
-  
-  return(
-    <div id='botbar'>
-    <span className='center-buttons'>
-      <button className='buttons'>INÍCIO</button>
-      <button className='buttons'>ÁREA DE RISCO</button>
-      <button className='buttons'>CLIMA E MARÉ</button>
-      <button className='buttons'>INFORMAÇÕES ÚTEIS</button>
-      <button className='buttons'>EMERGÊNCIA</button></span>
-      <img src={ouvidoria} alt="" />
-    </div>
-  )
-
-};
-
-
+import './App.css';
+import TopBar from './components/TopBar';
+import BotBar from './components/BotBar';
 
 const App = () => {
   return(
-    <><TopBar />
-    <BotBar /></>
+    <>
+    <TopBar />
+    <BotBar />
+    </>
   )
 };
 
