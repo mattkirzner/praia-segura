c20604dc0603ecfe170ebd563c6acd3fdeada475
felipecisneiros <fca@cesar.school>
2025-04-26 10:11:12 -0300

    Parte de Emergencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import * as React from 'react';
-import './App.css';
-import TopBar from './components/TopBar';
-import BotBar from './components/BotBar';
+import { Routes, Route } from 'react-router-dom';
+import Inicio from './Inicio';
+import AreaDeRisco from './AreaDeRisco';
+import ClimaEMare from './ClimaEMare';
+import InformacoesUteis from './InformacoesUteis';
+import Emergencia from './Emergencia'; 
 
-const App = () => {
-  return(
-    <>
-    <TopBar />
-    <BotBar />
-    </>
-  )
-};
-
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Inicio />} />
+      <Route path="/area-de-risco" element={<AreaDeRisco />} />
+      <Route path="/clima-e-mare" element={<ClimaEMare />} />
+      <Route path="/informacoes-uteis" element={<InformacoesUteis />} />
+      <Route path="/emergencia" element={<Emergencia />} /> 
+    </Routes>
+  );
+}
 
 export default App;
