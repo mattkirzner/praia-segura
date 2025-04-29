9ff9de222c6db8512cb437b02c6c9e6b2c5ea37b
Arthur Borba Lins <arthurborbalins@gmail.com>
2025-04-28 23:12:11 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,21 @@ import TopBar from './components/TopBar';
 import BotBar from './components/BotBar';
 import { Routes, Route } from 'react-router-dom';
 
-// Importe os componentes do dropdown diretamente da pasta components (nomes CORRIGIDOS)
+// Importação dos componentes
 import PaginaEcossistemaComponent from './components/Ecossistema';
 import PaginaTubaroesComponent from './components/PaginaTubaroes';
 import PaginaComoCurtirComponent from './components/PaginaComoCurtir';
 import PaginaEcoturismoComponent from './components/PaginaEcoturismo';
-import PergFreq from './components/PergFreq'; 
-import EsportesLazer from './components/EsportesLazer'
-
-// Importe a página de Ouvidoria
-import Ouvidoria from './components/Ouvidoria';  // Importe o componente Ouvidoria
+import PergFreq from './components/PergFreq';
+import EsportesLazer from './components/EsportesLazer';
+import Ouvidoria from './components/Ouvidoria';
 import Emergencia from './components/Emergencia';
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <BotBar /> {/* A BotBar deve vir ANTES do Routes */}
+      <BotBar />
       <Routes>
         {/* Rotas para os itens do dropdown */}
         <Route path="/Ecossistema" element={<PaginaEcossistemaComponent />} />
@@ -30,14 +28,12 @@ const App = () => {
         <Route path="/PergFreq" element={<PergFreq />} />
         <Route path="/EsportesLazer" element={<EsportesLazer />} />
         
-        {/* Outras rotas principais (Início, Área de Risco, etc.) */}
+        {/* Outras rotas principais */}
         <Route path="/" element={<div>Página Inicial</div>} />
         <Route path="/AreaDeRisco" element={<div>Área de Risco</div>} />
         <Route path="/ClimaEMare" element={<div>Clima e Maré</div>} />
         <Route path="/Emergencia" element={<Emergencia />} />
-        
-        {/* Rota da página de Ouvidoria */}
-        <Route path="/ouvidoria" element={<Ouvidoria />} /> {/* Adiciona a rota da Ouvidoria */}
+        <Route path="/Ouvidoria" element={<Ouvidoria />} />
       </Routes>
     </>
   );
