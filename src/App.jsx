67cb83b3087d0e04924d621a6a46a9b854a7c011
Arthur Borba Lins <arthurborbalins@gmail.com>
2025-04-28 22:15:50 -0300

    Atualizando App.jsx
    Incluindo as rotas para outras páginas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
+import * as React from 'react';
+import './App.css';
+import TopBar from './components/TopBar';
+import BotBar from './components/BotBar';
 import { Routes, Route } from 'react-router-dom';
-import Inicio from './Inicio';
-import AreaDeRisco from './AreaDeRisco';
-import ClimaEMare from './ClimaEMare';
-import InformacoesUteis from './InformacoesUteis';
-import Emergencia from './Emergencia'; 
 
-function App() {
+// Importe os componentes do dropdown diretamente da pasta components (nomes CORRIGIDOS)
+import PaginaEcossistemaComponent from './components/Ecossistema';
+import PaginaTubaroesComponent from './components/PaginaTubaroes';
+import PaginaComoCurtirComponent from './components/PaginaComoCurtir';
+import PaginaEcoturismoComponent from './components/PaginaEcoturismo';
+import PergFreq from './components/PergFreq'; 
+import EsportesLazer from './components/EsportesLazer'
+
+// Importe a página de Ouvidoria
+import Ouvidoria from './components/Ouvidoria';  // Importe o componente Ouvidoria
+import Emergencia from './components/Emergencia';
+
+const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Inicio />} />
-      <Route path="/area-de-risco" element={<AreaDeRisco />} />
-      <Route path="/clima-e-mare" element={<ClimaEMare />} />
-      <Route path="/informacoes-uteis" element={<InformacoesUteis />} />
-      <Route path="/emergencia" element={<Emergencia />} /> 
-    </Routes>
+    <>
+      <TopBar />
+      <BotBar /> {/* A BotBar deve vir ANTES do Routes */}
+      <Routes>
+        {/* Rotas para os itens do dropdown */}
+        <Route path="/Ecossistema" element={<PaginaEcossistemaComponent />} />
+        <Route path="/Tubaroes" element={<PaginaTubaroesComponent />} />
+        <Route path="/ComoCurtir" element={<PaginaComoCurtirComponent />} />
+        <Route path="/Ecoturismo" element={<PaginaEcoturismoComponent />} />
+        <Route path="/PergFreq" element={<PergFreq />} />
+        <Route path="/EsportesLazer" element={<EsportesLazer />} />
+        
+        {/* Outras rotas principais (Início, Área de Risco, etc.) */}
+        <Route path="/" element={<div>Página Inicial</div>} />
+        <Route path="/AreaDeRisco" element={<div>Área de Risco</div>} />
+        <Route path="/ClimaEMare" element={<div>Clima e Maré</div>} />
+        <Route path="/Emergencia" element={<Emergencia />} />
+        
+        {/* Rota da página de Ouvidoria */}
+        <Route path="/ouvidoria" element={<Ouvidoria />} /> {/* Adiciona a rota da Ouvidoria */}
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
