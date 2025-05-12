3ba7c5aa1d0918f2c0e4307b64b5549dde268698
felipecisneiros <fca@cesar.school>
2025-05-12 15:45:16 -0300

    Melhoria nas imagens, adicionei geolocalização e alteração nos textos

--- src/components/AreaDeRisco.jsx
+++ src/components/AreaDeRisco.jsx
@@ -1,43 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './AreaDeRisco.css'; 
-import background from '../imgs/background-area-de-risco.png'
+import background from '../imgs/background-area-de-risco.png';
 import mapa1 from '../imgs/mapa-1.png';
 import mapa2 from '../imgs/mapa-2.png';
 import mapa3 from '../imgs/mapa-3.png';
 
 function AreaDeRisco() {
- const textoMapa1 = `PRAIA DE BOA VIAGEM: aconselhado o banho de mar apenas na maré-baixa e em áreas de arrecifes` 
+  const [userLocation, setUserLocation] = useState(null);
 
- const textoMapa2 = `PRAIA DE PIEDADE (IGREJINHA) - HOTEL BARRAMARES: banho de mar proibido no trecho` 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation(`https://www.google.com/maps?q=${latitude},${longitude}`);
+      },
+      (error) => {
+        console.error('Erro ao obter localização:', error);
+      }
+    );
+  }, []);
 
- const textoMapa3 = `PRAIA DO PAIVA - PRAIA DE ITAPUAMA: aconselhado o banho de mar apenas na maré baixa e em áreas de arrecifes` 
- 
- return (
+  const tituloMapa1 = `PRAIA DE BOA VIAGEM:`;
+  const textoMapa1 = `Aconselhado o banho de mar apenas na maré-baixa e em áreas de arrecifes`;
+
+  const tituloMapa2 = `PRAIA DE PIEDADE (IGREJINHA) - HOTEL BARRAMARES:`;
+  const textoMapa2 = `Banho de mar proibido no trecho`;
+
+  const tituloMapa3 = `PRAIA DO PAIVA - PRAIA DE ITAPUAMA:`; 
+  const textoMapa3 = `Aconselhado o banho de mar apenas na maré baixa e em áreas de arrecifes`;
+
+  return (
     <div className="areaDeRisco-container" style={{ backgroundImage: `url(${background})` }}>
+      
 
-        
-    <div className="container">
+      {userLocation && (
+        <a
+          href={userLocation}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="map-link"
+        >
+          📍VOCÊ ESTÁ AQUI!
+        </a>
+      )}
+
+      <div className="container">
         <img src={mapa1} alt="PRAIA DE BOA VIAGEM" className="imagem" />
-        <div className='contatos-box'>
-        <p className="texto">{textoMapa1}</p>
+        <div className="contatos-box">
+          <h1 className="texto">{tituloMapa1}</h1>
+          <h3 className="texto">{textoMapa1}</h3>
         </div>
-    </div>
+      </div>
 
-    <div className="container">
+      <div className="container">
         <img src={mapa2} alt="PRAIA DE PIEDADE (IGREJINHA)" className="imagem" />
-        <div className='contatos-box'>
-        <p className="texto">{textoMapa2}</p>
+        <div className="contatos-box">
+          <h1 className="texto">{tituloMapa2}</h1>
+          <h3 className="texto">{textoMapa2}</h3>
         </div>
-    </div>
+      </div>
 
-    <div className="container">
+      <div className="container">
         <img src={mapa3} alt="PRAIA DO PAIVA" className="imagem" />
-        <div className='contatos-box'>
-        <p className="texto">{textoMapa3}</p>
+        <div className="contatos-box">
+          <h1 className="texto">{tituloMapa3}</h1>
+          <h3 className="texto">{textoMapa3}</h3>
         </div>
-    </div>
-
-
+      </div>
     </div>
   );
 }
